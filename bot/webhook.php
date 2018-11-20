906b21669f7e95454aa276d24bb4cf2fcec06e95
Arius1404 <tahteev.ilya@gmail.com>
2018-11-20 20:28:50 +0200

    message /help v1

--- bot/webhook.php
+++ bot/webhook.php
@@ -9,8 +9,10 @@ $response = json_decode($response, true);
 $chat_id = $response['message']['chat']['id'];
 $msg = $response['message']['text'];
 $username = $response['message']['chat']['username'];
-if($msg='/start'){
+if($msg=='/start'){
 	$msg = $username . ' добро пожаловать в бота! Нажмите /help для отоблажения доступных команд!';
+}elseif($msg=='/help'){
+	$msg = "/img - get image;\r\n/jpg - get jpg";
 }
 $url = 'https://api.telegram.org/bot750970720:AAGe_QVL0D9pXwtNK2Lzx8LBZtrROdSLGQE/sendMessage?chat_id='.$chat_id.'&text='.$msg;
 $responce_message = file_get_contents($url);
