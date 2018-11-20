2d6a72a8c0aba20af332948178dac863a8c403a0
Arius1404 <tahteev.ilya@gmail.com>
2018-11-20 15:49:25 +0200

    test message

--- bot/webhook.php
+++ bot/webhook.php
@@ -1,5 +1,12 @@
 <?php
+//750970720:AAGe_QVL0D9pXwtNK2Lzx8LBZtrROdSLGQE
 $response = file_get_contents('php://input');
-$msgs = file_get_contents('tmp/messages_log.txt');
+$msgs = file_get_contents('messages_log.txt');
 $msgs .= "\r\n".$response;
-file_put_contents('tmp/messages_log.txt', $msgs);
+file_put_contents('messages_log.txt', $msgs);
+$response = json_decode($response, true);
+$chat_id = $response['message']['chat']['id'];
+$msg = $response['message']['text'];
+$url = 'https://api.telegram.org/bot750970720:AAGe_QVL0D9pXwtNK2Lzx8LBZtrROdSLGQE/sendMessage?chat_id='.$chat_id.'&text='.$msg;
+$responce_message = file_get_contents($url);
+file_put_contents('resp_message.txt', $responce_message);
