2d9e297998253a4ad3e42e3d13032472da3be5ba
Arius1404 <tahteev.ilya@gmail.com>
2018-12-02 01:21:09 +0700

    Description added

--- pages/about.php
+++ pages/about.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<h1 class="h3 mb-3 mt-3 font-weight-normal text-primary text-center">О нас</h1>
 	<div class="row">
-		<p>Данный сайт выполнен студентами группы 716 в качестве курсовой работы по предмету "Технологии и методы программирования"</p>
+		<p align="center"><font face="Verdana, Geneva, sans-serif">Данный сайт выполнен студентами группы 716 в качестве курсовой работы по предмету &quot;Технологии и методы программирования&quot;</font></p>
 	</div>
 </div>
